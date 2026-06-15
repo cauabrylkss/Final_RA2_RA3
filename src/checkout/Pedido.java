b3e98b6ce773e0f366644f57f3e3879a29ff5705
Migliorini <vicentemigliorini2007@gmail.com>
2026-06-15 18:54:41 -0300

    feat: adicionada a classe de pedido

--- src/checkout/Pedido.java
+++ src/checkout/Pedido.java
@@ -1,23 +1,37 @@
 package checkout;
+
+import Produtos.Produto;
+
 public class Pedido {
 
     protected int id;
-    protected String cnpj;
-    protected int preco;
-    protected String forma_macarrao;
-    protected float quantidade;
+    protected String cnpjCliente;
+    private Produto produto;
+    private double quantidade;
     protected StatusPedido statusPedido;
 
-    Pedido(String cnpj, int preco, String forma_macarrao, float quantidade){
-        this.cnpj = cnpj;
-        this.preco = preco;
-        this.forma_macarrao = forma_macarrao;
+    Pedido(String cnpjCliente, Produto produto, double quantidade){
+        this.cnpjCliente = cnpjCliente;
+        this.produto = produto;
         this.quantidade = quantidade;
         this.statusPedido = StatusPedido.PEDIDO;
     }
 
-    public void iniciarFabricacao(){}
+    public int     getId()      { return id; }
+    public String  getCnpjCliente() { return cnpjCliente; }
+    public Produto getProduto()     { return produto; }
+    public double  getQuantidade()  { return quantidade; }
 
-    public void iniciarEntrega(){}
+    public double getValorItem() {
+        return produto.getPrecoPorKg() * quantidade;
+    }
 
+    @Override
+    public String toString() {
+        return "Pedido #" + this.id + "CNPJ: "
+                + cnpjCliente + produto.getForma()
+                + quantidade + " kg"
+                + "Preco/Kg: " + String.format("%.2f", produto.getPrecoPorKg())
+                + "Total: R$ " + String.format("%.2f", getValorItem());
+    }
 }
