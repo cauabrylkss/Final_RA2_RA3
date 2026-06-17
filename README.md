fe04c5b7305511d02b7087d257f198f5716b7292
Migliorini <vicentemigliorini2007@gmail.com>
2026-06-17 20:53:27 -0300

    comentario de resumo do fluxo do sistema foi para o readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 rm .\src\pedidos_semana1Escrita.csv,  .\src\pedidos_semana2Escrita.csv,  .\src\pedidos_semana3Escrita.csv,  .\src\pedidos_semana4Escrita.csv, .\log_canceladosSemana2,  .\log_canceladosSemana3,  .\log_canceladosSemana4, .\log_entregasSemana3,  .\log_entregasSemana4, .\log_fabricacaoSemana1, .\log_fabricacaoSemana2, .\log_fabricacaoSemana3, .\log_fabricacaoSemana4,  .\dados.dat
+
+// P1
+//├── LeitorClientes   → clientes.csv
+//├── LeitorPedidos    → pedidos_semana1.csv.csv (mín. 10 pedidos) 4 chamadas em p1 assim: new LeitorPedidos(clientes, "pedidos_semana1.csv", "registros_semana1.csv").exportar();
+//├── LeitorPedidos    → pedidos_semana2.csv.csv (mín. 10 pedidos)
+//├── LeitorPedidos    → pedidos_semana3.csv.csv (mín. 10 pedidos)
+//├── LeitorPedidos    → pedidos_semana4.csv.csv (mín. 10 pedidos)
+//└── serializa tudo   → dados.dat
+//
+//P2
+//├── desserializa dados.dat
+//├── simula semana 1 → EscritorFabricacao, EscritorCancelados
+//├── simula semana 2 → EscritorFabricacao, EscritorCancelados, EscritorEntregas
+//├── simula semana 3 → idem
+//├── simula semana 4 → idem
+//└── EscritorLog     → ao longo de tudo
--- src/P1.java
+++ src/P1.java
@@ -14,24 +14,6 @@ import usuarios.Cliente;
 public class P1{
     public static void main(String[] args) throws IOException {
 
-        // FLUXO
-
-        // P1
-        //├── LeitorClientes   → clientes.csv
-        //├── LeitorPedidos    → pedidos_semana1.csv.csv (mín. 10 pedidos) 4 chamadas em p1 assim: new LeitorPedidos(clientes, "pedidos_semana1.csv", "registros_semana1.csv").exportar();
-        //├── LeitorPedidos    → pedidos_semana2.csv.csv (mín. 10 pedidos)
-        //├── LeitorPedidos    → pedidos_semana3.csv.csv (mín. 10 pedidos)
-        //├── LeitorPedidos    → pedidos_semana4.csv.csv (mín. 10 pedidos)
-        //└── serializa tudo   → dados.dat
-        //
-        //P2
-        //├── desserializa dados.dat
-        //├── simula semana 1 → EscritorFabricacao, EscritorCancelados
-        //├── simula semana 2 → EscritorFabricacao, EscritorCancelados, EscritorEntregas
-        //├── simula semana 3 → idem
-        //├── simula semana 4 → idem
-        //└── EscritorLog     → ao longo de tudo
-
         LeitorClientes leitorClientes = new LeitorClientes("src/clientes.csv");
         ArrayList<Cliente> clientes = leitorClientes.getLista_clientes();
         ArrayList<ArrayList<Pedido>> pedidosPorSemana = new ArrayList<>();
